283b93835804b849299ec3c336207348cd305f6a
Alan Shaw <alan.shaw@protocol.ai>
2022-12-10 00:39:32 +0000

    feat: up command (#7)

--- lib.js
+++ lib.js
@@ -1,5 +1,22 @@
 import fs from 'fs'
+import path from 'path'
 
 export function getPkg () {
   return JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url)))
+}
+
+export function checkPathsExist (paths) {
+  paths = Array.isArray(paths) ? paths : [paths]
+  for (const p of paths) {
+    if (!fs.existsSync(p)) {
+      console.error(`The path ${path.resolve(p)} does not exist`)
+      process.exit(1)
+    }
+  }
+  return paths
+}
+
+export function filesize (bytes) {
+  const size = bytes / 1024 / 1024
+  return `${size.toFixed(1)}MB`
 }
