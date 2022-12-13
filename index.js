c849d8e2699ac32f4babc661a9b1a8267803cc54
Alan Shaw <alan.shaw@protocol.ai>
2022-12-13 23:02:54 +0000

    fix: files-from-path with common path prefix removed (#27)
    I think this is what we want.
    
    This PR adds a new function `filesFromPaths` that returns a list of File
    objects with common path prefix removed. i.e. in this example `test` has
    been removed:
    
    1. <img width="949" alt="Screenshot 2022-12-13 at 15 46 30"
    src="https://user-images.githubusercontent.com/152863/207379552-593159d0-0efe-4539-875a-a6fc7fe12f87.png">
    
    2. <img width="1028" alt="Screenshot 2022-12-13 at 15 54 32"
    src="https://user-images.githubusercontent.com/152863/207381138-feb67dd7-86f3-4ac6-8adf-c55eab82172c.png">
    
    3. <img width="783" alt="Screenshot 2022-12-13 at 15 53 00"
    src="https://user-images.githubusercontent.com/152863/207380777-12c1c1cc-5709-4a74-942b-e00080455945.png">
    
    No commonality:
    
    4. <img width="1130" alt="Screenshot 2022-12-13 at 15 55 52"
    src="https://user-images.githubusercontent.com/152863/207381449-84ba38f1-abd0-48cb-8c7c-cf3ea4e9fe52.png">
    
    **It means that you only ever need `--no-wrap` when there's 1 file**. In
    the case of multiple files, they are either in different directories and
    so _must_ be wrapped (1/2), or they are in the same directory, their
    common prefix is removed and they _must_ be wrapped (3).
    
    It's also consistent with the `FileList` you'd receive from a HTML
    `<input type="file" multiple/>`.
    
    Added bonus is that this function returns `File` objects whose
    `stream()` method returns a web `ReadableStream` not a Node.js one, so
    no need to convert. It also removes globbing dependencies,
    `util.promisify` and and bunch of other functionality we're not using
    from `files-from-path`.
    
    Co-authored-by: Oli Evans <oli@protocol.ai>

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ import tree from 'pretty-tree'
 import { Readable } from 'stream'
 import * as DID from '@ipld/dag-ucan/did'
 import { CarWriter } from '@ipld/car'
-import { filesFromPath } from 'files-from-path'
-import { getClient, checkPathsExist, filesize, readProof } from './lib.js'
+import { getClient, checkPathsExist, filesize, readProof, filesFromPaths } from './lib.js'
 
 /**
  * @param {string} firstPath
@@ -17,17 +16,11 @@ export async function upload (firstPath, opts) {
   const paths = checkPathsExist([firstPath, ...opts._])
   const client = await getClient()
   const hidden = !!opts.hidden
-  const files = []
-  let totalSize = 0
   let totalSent = 0
-  const spinner = ora('Packing files').start()
-  for (const p of paths) {
-    for await (const file of filesFromPath(p, { hidden })) {
-      totalSize += file.size
-      files.push({ name: file.name, stream: () => Readable.toWeb(file.stream()) })
-      spinner.text = `Packing ${files.length} file${files.length === 1 ? '' : 's'} (${filesize(totalSize)})`
-    }
-  }
+  const spinner = ora('Reading files').start()
+  const files = await filesFromPaths(paths, { hidden })
+  const totalSize = files.reduce((total, f) => total + f.size, 0)
+  spinner.stopAndPersist({ text: `${files.length} file${files.length === 1 ? '' : 's'} (${filesize(totalSize)})` })
   spinner.start('Storing')
   // @ts-ignore
   const root = await client.uploadDirectory(files, {
