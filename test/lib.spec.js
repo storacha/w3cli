1b69cbb06bc7987d89aa79dd155b112a4405950a
Alan Shaw <alan.shaw@protocol.ai>
2023-03-22 14:04:57 +0000

    refactor: use new `files-from-path` module (#55)
    The `files-from-path` module has been updated to do what the code here
    was doing.
    
    TLDR; web streams and automatic common prefix removal.
    
    More info: https://github.com/web3-storage/files-from-path/pull/23

--- test/lib.spec.js
+++ test/lib.spec.js
@@ -1,37 +1,10 @@
 import test from 'ava'
 import * as CID from 'multiformats/cid'
 import {
-  filesFromPaths,
   filesize,
   uploadListResponseToString,
   storeListResponseToString
 } from '../lib.js'
-
-test('filesFromPaths', async (t) => {
-  const files = await filesFromPaths(['node_modules'])
-  t.log(`${files.length} files in node_modules`)
-  t.true(files.length > 1)
-})
-
-test('filesFromPaths includes file size', async (t) => {
-  const files = await filesFromPaths(['test/fixtures/empty.car'])
-  t.is(files.length, 1)
-  t.is(files[0].size, 18)
-})
-
-test('filesFromPaths removes common path prefix', async (t) => {
-  const files = await filesFromPaths(['test/fixtures', './test/helpers'])
-  t.true(files.length > 1)
-  for (const file of files) {
-    t.false(file.name.startsWith('test/'))
-  }
-})
-
-test('filesFromPaths single file has name', async (t) => {
-  const files = await filesFromPaths(['test/fixtures/empty.car'])
-  t.is(files.length, 1)
-  t.is(files[0].name, 'empty.car')
-})
 
 test('filesize', t => {
   [
