21324283411a693ff0bd1bb7580d10dfa5c15f68
Alan Shaw <alan@storacha.network>
2025-02-24 16:59:04 +0000

    feat: github login

--- account.js
+++ account.js
@@ -1,6 +1,10 @@
+import open from 'open'
+import { confirm } from '@inquirer/prompts'
 import * as Account from '@web3-storage/w3up-client/account'
 import * as Result from '@web3-storage/w3up-client/result'
 import * as DidMailto from '@web3-storage/did-mailto'
+import { authorize } from '@web3-storage/capabilities/access'
+import { base64url } from 'multiformats/bases/base64'
 import { getClient } from './lib.js'
 import ora from 'ora'
 
@@ -8,10 +12,26 @@ import ora from 'ora'
  * @typedef {Awaited<ReturnType<Account.login>>['ok']&{}} View
  */
 
+const OAuthProviderGitHub = 'github'
+const OAuthProviders = /** @type {const} */ ([OAuthProviderGitHub])
+
+const githubOAuthClientID = process.env.GITHUB_OAUTH_CLIENT_ID || 'Ov23liRdyizj8EndxxAf'
+
 /**
- * @param {DidMailto.EmailAddress} email
+ * @param {DidMailto.EmailAddress} [email]
+ * @param {object} [options]
+ * @param {boolean} [options.github]
  */
-export const login = async (email) => loginWithClient(email, await getClient())
+export const login = async (email, options) => {
+  if (email) {
+    await loginWithClient(email, await getClient())
+  } else if (options?.github) {
+    await oauthLoginWithClient(OAuthProviderGitHub, await getClient())
+  } else {
+    console.error('Error: please provide email address or specify flag for alternate login method')
+    process.exit(1)
+  }
+}
 
 /**
  * @param {DidMailto.EmailAddress} email
@@ -40,6 +60,57 @@ export const loginWithClient = async (email, client) => {
     process.exit(1)
   } finally {
     clearTimeout(timeout)
+  }
+}
+
+/**
+ * @param {(typeof OAuthProviders)[number]} provider OAuth provider
+ * @param {import('@web3-storage/w3up-client').Client} client
+ */
+export const oauthLoginWithClient = async (provider, client) => {
+  if (provider != OAuthProviderGitHub) {
+    console.error(`Error: unknown OAuth provider: ${provider}`)
+    process.exit(1)
+  }
+
+  /** @type {import('ora').Ora|undefined} */
+  let spinner
+
+  try {
+    const delegation = await authorize.delegate({
+      audience: client.agent.connection.id,
+      issuer: client.agent.issuer,
+      with: client.agent.did(),
+      nb: { att: [{ can: '*' }] }
+    })
+    const archive = await delegation.archive()
+    if (archive.error) {
+      throw new Error('archiving access authorize delegation', { cause: archive.error })
+    }
+
+    const clientID = githubOAuthClientID
+    const state = base64url.encode(archive.ok)
+    const loginURL = `https://github.com/login/oauth/authorize?scope=read:user,user:email&client_id=${clientID}&state=${state}`
+
+    if (await confirm({ message: 'Open the GitHub login URL in your default browser?' })) {
+      spinner = ora('Waiting for GitHub authorization to be completed in browser...').start()
+      await open(loginURL)
+    } else {
+      spinner = ora(`Click the link to authenticate with GitHub: ${loginURL}`).start()
+    }
+
+    const expiration = Math.floor(Date.now() / 1000) + (60 * 15)
+    const account = Result.unwrap(await Account.externalLogin(client, { request: delegation.cid, expiration }))
+
+    Result.unwrap(await account.save())
+
+    if (spinner) spinner.stop()
+    console.log(`⁂ Agent was authorized by ${account.did()}`)
+    return account
+  } catch (err) {
+    if (spinner) spinner.stop()
+    console.error(err)
+    process.exit(1)
   }
 }
 
